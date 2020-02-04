87ab0131ccdd87ce0292416fc8a9c3b53a54003e
yoann.masson95@gmail.com <yoann.masson@infotel.com>
2020-02-04 14:54:45 +0100

    add win condition

--- src/App.js
+++ src/App.js
@@ -26,16 +26,22 @@ class App extends Component{
   render = () =>{
     return (
       <div className="jeu">
-        <Clavier handleClickOnChar={this.handleClickOnChar} usedLetters={this.state.usedLetters} />
-        <p> {this.computeDisplay(this.state.secret,this.state.usedLetters)}</p>
-        <p> Il vous reste {this.state.nbTryLeft} essai{this.state.nbTryLeft > 1 && "s" }</p>
+        { !this.state.won && (
+          <div>
+            <Clavier handleClickOnChar={this.handleClickOnChar} usedLetters={this.state.usedLetters} />
+            <p> {this.computeDisplay(this.state.secret,this.state.usedLetters)}</p>
+            <p> Il vous reste {this.state.nbTryLeft} essai{this.state.nbTryLeft > 1 && "s" }</p>
+          </div>
+          )
+        }
+        {this.state.won && "BRAVO: " + this.state.secret}
         {this.state.nbTryLeft === 0 && this.state.secret}
         <button onClick={() => this.resetGame()}>reset</button>
       </div> 
     );
   }
 
-  resetGame = () =>{
+  resetGame(){
     this.setState(DEFAULT_STATE)
     this.setState ({
       secret: RandomWords().toLocaleUpperCase()
@@ -43,19 +49,36 @@ class App extends Component{
   }
 
   handleClickOnChar = char => {
+
     if(this.state.nbTryLeft > 0){
       if(!this.state.secret.includes(char.char)){
         this.setState ({ 
           nbTryLeft: this.state.nbTryLeft-1 ,
           usedLetters: [...this.state.usedLetters,char.char]
-        })
+        },() => this.checkWinning())
       }else{
         this.setState ({ 
           usedLetters: [...this.state.usedLetters,char.char]
-        })
+        },() => this.checkWinning())
+        
       }
-     
     }
+  }
+
+  //Last character
+  checkWinning = () =>{
+
+    var charArray = this.state.secret.split("")
+    var missingLetter = false;
+    charArray.forEach(letter => {
+      if(!this.state.usedLetters.includes(letter)){
+        missingLetter = true;
+      }
+    });
+    this.setState({
+      won: !missingLetter
+    })
+
   }
 
 
